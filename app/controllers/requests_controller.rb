91835493f7896b97e04440f1888d92e06febfb68
augustin brun brun.aub1512@gmail.com <brun.aub1512@gmail.com>
2020-02-25 12:58:34 -0500

    Simple form to request and watch

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,21 @@
 class RequestsController < ApplicationController
+  def new
+    @request = Request.new
+  end
+
+  def create
+    @request = request.find(safe_params)
+    @request.watch = watch
+
+    if @request.save
+      redirect_to user_path(@request)
+    else
+      render :new
+    end
+
+  end
+
+  def safe_params
+    params.require(:request).permit(:date_start, :date_end)
+  end
 end
--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -2,29 +2,37 @@ class WatchesController < ApplicationController
 
 
   def index
-    # @watches = Watch.all
+    @watches = Watch.all
   end
 
   def show
-    # @watch = Watch.find(params[:id])
+    @watch = Watch.find(params[:id])
   end
 
   def new
-    # @watch = Watch.new
+    @watch = Watch.new
   end
 
   def create
-    # @watch = Watch.new(safe_params)
+    @watch = Watch.new(safe_params)
 
-    #   if @watch.save
-    #   else
-    #     render :new
-    #   end
+    if @watch.save
+      redirect_to user_path(@watch)
+    else
+         render :new
+       end
+  end
+
+  def edit
+
+  end
+
+  def update
   end
 
   private
 
   def safe_params
-    # params.require(:watch).permit(:brand, :model, :price, :material)
+    params.require(:watch).permit(:brand, :model, :price, :material)
   end
 end
