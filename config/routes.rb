4404118c31441a05c42cd13a2492930803e8748b
Mohammad Mirza <Hammad_Senior@hotmail.com>
2020-02-24 20:08:18 -0500

    Attempting to resolve routes issue, was unsuccesful. Will try again.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # resources :watch only: [:index, :show]
+
 
   resources :users do
     resources :watches, only: [:new, :create, :edit, :update, :delete]
