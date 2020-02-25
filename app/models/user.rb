f26b7fb7df89ed8f610a18b9ecd63d91aa2fb3b3
augustin brun brun.aub1512@gmail.com <brun.aub1512@gmail.com>
2020-02-24 19:46:25 -0500

    add model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :first_name, presence: true, uniqueness: true
+  validates :last_name, presence: true, uniqueness: true
+  validates :address, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- app/models/watch.rb
+++ app/models/watch.rb
@@ -1,3 +1,7 @@
 class Watch < ApplicationRecord
-
+  belongs_to :user
+  validates :model, presence: true
+  validates :brand, presence: true
+  validates :material, presence: true
+  validates :price, presence: true, numericality: true
 end
