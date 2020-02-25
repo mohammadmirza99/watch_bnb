4c3e7255697b4e7f92282701c5b8138ae52f5cef
augustin brun brun.aub1512@gmail.com <brun.aub1512@gmail.com>
2020-02-25 15:11:57 -0500

    setup cloudinary / add migration to cloudinary / Model

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -2,6 +2,7 @@ class Watch < ApplicationRecord
   belongs_to :user
   has_many :reviews
   has_many :requests
+  has_one_attached :image_url
   validates :model, presence: true
   validates :brand, presence: true
   validates :material, presence: true
