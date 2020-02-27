81ecd2bf4ff1eea2436ca82b545fc80a0134b737
Mohammad Mirza <Hammad_Senior@hotmail.com>
2020-02-27 16:32:20 -0500

    working on reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,12 @@
 class ReviewsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @watch = Watch.find(params[:watch_id])
+    @reviews = Review.all
+  end
+
+  def new
+    @watch = Watch.find(params[:watch_id])
+  end
 end
