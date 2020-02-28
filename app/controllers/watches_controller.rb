877a1ba0f349dcf6b19aea739bcb621bd1218371
Mohammad Mirza <Hammad_Senior@hotmail.com>
2020-02-28 16:10:46 -0500

    Fixed links

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -22,7 +22,7 @@ class WatchesController < ApplicationController
     # authorize @watch
     @watch.user = current_user
     if @watch.save
-      redirect_to watches_path
+      redirect_to user_path(current_user)
     else
       render :new
     end
